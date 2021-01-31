1b20363d41538a5d4f48111990e2cb30e7f85771
Hush-poop <71926032+Hush-poop@users.noreply.github.com>
2021-01-31 15:00:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Fortnie
+# synchronousBallMovement
+Ball moving synchronously
---
+++ sketch.js
@@ -0,0 +1,14 @@
+var backgroundImage
+var gamestate=0;
+var playerCount
+var database
+var form,player,game;
+
+function setup(){
+  createCanvas(400,400)
+  database=firebase.database();
+}
+
+function draw(){
+  
+}
